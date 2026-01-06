a37fff614e6dc02530a8b99ba628b3c6d93c4685
julio gabriel <juliogabriel78956@gmail.com>
2026-01-06 15:40:28 -0300

    Associção de dominio

---
+++ src/AssociacaoDeDominio/Test02/JogadorTest01.java
@@ -0,0 +1,17 @@
+package AssociacaoDeDominio.Test02;
+
+import AssociacaoDeDominio.Dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Neymar");
+        Jogador jogador2 = new Jogador("Cr7");
+        Jogador jogador3 = new Jogador("Messi");
+        Jogador[]jogadores = {jogador1,jogador2,jogador3};
+
+        for (Jogador jogador : jogadores) {
+           jogador.imprime();
+        }
+
+    }
+}
