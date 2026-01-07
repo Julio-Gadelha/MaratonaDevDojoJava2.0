1b45408c73e024d4b2572b4240cc1a9f02e41593
julio gabriel <juliogabriel78956@gmail.com>
2026-01-07 15:55:07 -0300

    Herença

---
+++ src/HerencaDominio/Dominio/Funcionario.java
@@ -0,0 +1,25 @@
+package HerencaDominio.Dominio;
+
+public class Funcionario extends  Pessoa{
+    private double salario;
+
+    public Funcionario(String nome){
+        super(nome);
+    }
+
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
+
+    public void relatorioaPagamento(){
+        System.out.println("Eu " +this.nome + "Recebi o salrio de "+this.salario);
+    }
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
