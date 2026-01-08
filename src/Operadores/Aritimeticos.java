082e9015421fc56c162776199df2dd09dbc98bfe
julio gabriel <juliogabriel78956@gmail.com>
2026-01-08 15:46:59 -0300

    Learn

---
+++ src/Operadores/Aritimeticos.java
@@ -0,0 +1,11 @@
+package Operadores;
+
+public class Aritimeticos {
+    public static void main(String[] args) {
+        // + - / * %
+
+        int numero2 =44;
+        int numero4=2902;
+        System.out.println(numero4 + numero2);
+    }
+}
---
+++ src/TiposPrimitivos/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package TiposPrimitivos;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        // Tipos de primitivos
+        // int,double,float,char,byte,short,long,boolean
+        int age = 34;
+        double salary = 10000.00;
+        float salary2 = 30000.00F;
+        char sexo = 'M';
+        byte number = 39;
+        short number2 = 98;
+        boolean or = true;
+        long numeroGrande = 492849;
+
+    }
+}
