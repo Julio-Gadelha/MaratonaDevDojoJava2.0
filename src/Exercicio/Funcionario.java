4230d5e2bb26996837cb9a619d75e0eb13ae41af
julio gabriel <juliogabriel78956@gmail.com>
2026-03-18 15:20:48 -0300

    Encapsulamento

---
+++ src/Exercicio/Funcionario.java
@@ -0,0 +1,16 @@
+package Exercicio;
+
+public class Funcionario {
+    public String nome;
+    public int idade ;
+     public double [] salario;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.salario);
+        for(double salario : salario){
+            System.out.println(salario + " ");
+        }
+    }
+}
---
+++ src/Exercicio/Tests/TestFuncionario.java
@@ -0,0 +1,12 @@
+package Exercicio.Tests;
+
+import Exercicio.Funcionario;
+
+public class TestFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome ="Gabriel";
+        funcionario.idade = 18;
+        funcionario.salario = new double[]{3000,5000,7000};
+    }
+}
