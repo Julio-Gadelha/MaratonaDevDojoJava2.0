9384af4b4149882044b402fe1ebe7adea1cd7ad5
julio gabriel <juliogabriel78956@gmail.com>
2026-05-03 11:35:21 -0300

    Heranca

---
+++ src/AssociacaoHeranca/Endereco.java
@@ -0,0 +1,24 @@
+package AssociacaoHeranca;
+
+public class Endereco  extends  Pessoa{
+    private String rua;
+    private String cep;
+
+
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
