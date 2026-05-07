7ff44c43f4155642c0c7df37d90075e6b767c6cc
julio gabriel <juliogabriel78956@gmail.com>
2026-05-07 11:30:53 -0300

    Modificador Final

---
+++ src/HerencaDominio/exerciciosHeranca/Dominio/Carro.java
@@ -0,0 +1,11 @@
+package HerencaDominio.exerciciosHeranca.Dominio;
+
+public class Carro  extends Veiculo {
+
+    @Override
+    void ligar() {
+        System.out.println("Carro ligado ! ");
+    }
+
+
+}
---
+++ src/HerencaDominio/exerciciosHeranca/Dominio/TestVeiculos.java
@@ -0,0 +1,15 @@
+package HerencaDominio.exerciciosHeranca.Dominio;
+
+public class TestVeiculos   {
+
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.ligar();
+
+        Moto moto = new Moto();
+        moto.ligar();
+
+
+    }
+}
