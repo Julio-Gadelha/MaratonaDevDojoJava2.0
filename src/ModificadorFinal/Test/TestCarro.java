892569c909720ff5163102026e9d3ce6709ad9b1
julio gabriel <juliogabriel78956@gmail.com>
2026-05-07 11:14:21 -0300

    Modificador Final

---
+++ src/ModificadorFinal/Test/TestCarro.java
@@ -0,0 +1,12 @@
+package ModificadorFinal.Test;
+
+import ModificadorFinal.Carro;
+
+public class TestCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.setNome("Porsche  911 GT3 rs");
+        System.out.println(carro.VELOCIDADE_LIMITE);
+
+    }
+}
